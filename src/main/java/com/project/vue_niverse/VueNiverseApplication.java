93d7dff729cdfe2edcc435909186afc1e5a58cee
unknown <1114ny@naver.com>
2025-08-14 08:31:38 +0900

    init project

---
+++ src/main/java/com/project/vue_niverse/VueNiverseApplication.java
@@ -0,0 +1,13 @@
+package com.project.vue_niverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueNiverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueNiverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/vue_niverse/VueNiverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vue_niverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueNiverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
