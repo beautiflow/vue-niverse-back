018d339a65cf98001833b0ccca381646ae6bf3f1
nayeon <1114ny@naver.com>
2025-08-14 17:01:30 +0900

    modify boardDto

--- src/main/java/com/project/vue_niverse/dto/BoardDto.java
+++ src/main/java/com/project/vue_niverse/dto/BoardDto.java
@@ -1,11 +1,8 @@
 package com.project.vue_niverse.dto;
 
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import lombok.Setter;
 
 import java.util.Date;
