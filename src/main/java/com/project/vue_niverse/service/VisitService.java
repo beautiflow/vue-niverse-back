5bb2d0bef5383b685915bdfcb5c94fa639352a87
nayeon <1114ny@naver.com>
2025-08-19 15:14:36 +0900

    refactor: modify count visitor

--- src/main/java/com/project/vue_niverse/service/VisitService.java
+++ src/main/java/com/project/vue_niverse/service/VisitService.java
@@ -4,5 +4,5 @@ public interface VisitService {
 
     int countVisits();
 
-    int plusVisits();
+    void plusVisits();
 }
--- src/main/java/com/project/vue_niverse/service/VisitServiceImpl.java
+++ src/main/java/com/project/vue_niverse/service/VisitServiceImpl.java
@@ -2,14 +2,11 @@ package com.project.vue_niverse.service;
 
 import com.project.vue_niverse.mapper.VisitMapper;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class VisitServiceImpl implements VisitService {
-    private static final Logger _log = LoggerFactory.getLogger(VisitServiceImpl.class);
 
     private final VisitMapper visitMapper;
 
@@ -19,7 +16,7 @@ public class VisitServiceImpl implements VisitService {
     }
 
     @Override
-    public int plusVisits() {
-        return visitMapper.plusVisits();
+    public void plusVisits() {
+        visitMapper.plusVisits();
     }
 }
