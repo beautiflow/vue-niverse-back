473fd286e5c81f6c11829a40cd568804495994c9
nayeon <1114ny@naver.com>
2025-08-19 10:11:07 +0900

    chore: refactor code

--- src/main/java/com/project/vue_niverse/service/BoardServiceImpl.java
+++ src/main/java/com/project/vue_niverse/service/BoardServiceImpl.java
@@ -3,8 +3,6 @@ package com.project.vue_niverse.service;
 import com.project.vue_niverse.dto.BoardDto;
 import com.project.vue_niverse.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
